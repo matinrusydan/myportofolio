a597937dbfa1c7d05ab6eadbbe1aa131389d178d
Matin Rusydan <matin.rusydan@gmail.com>
2025-06-06 02:51:45 +0700

    `Update Filament dashboard panel colors to Purple`

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -28,7 +28,7 @@ class DashboardPanelProvider extends PanelProvider
             ->path('dashboard')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Purple,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->resources([
